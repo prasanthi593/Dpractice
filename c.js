a2552f850f75ac20cb6d170c64928441683c0bfa
prasanthi593 <prasanthi.kotikalapudi@sasi.ac.in>
2024-04-22 19:51:12 +0530

    first commit

---
+++ c.js
@@ -0,0 +1,9 @@
+
+document.getElementById("registrationForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent form submission
+  
+    // Perform form submission handling (e.g., sending data to server)
+    // For demonstration purposes, just showing an alert
+    alert("Successfully submitted!");
+  });
+  
